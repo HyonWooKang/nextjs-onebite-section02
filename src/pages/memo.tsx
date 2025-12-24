1fb0354eccab0596c69b9d6121f30a9607c19f84
HyonWooKang <87benzenes@gmail.com>
2025-12-24 20:56:38 +0900

    공부한 내역 정리

---
+++ src/pages/memo.tsx
@@ -0,0 +1,19 @@
+export default function MemoPage() {
+  return (
+    <>
+      <section>
+        <div>
+          <h2>공부한 내용</h2>
+        </div>
+        <div>
+          <ol>
+            <li>Routing - Routing, Query String, dynamic address</li>
+            <li>Routing - prefetching 관리</li>
+            <li>API Routes - 잘 사용 안함</li>
+            <li>Style - CSS 모듈화</li>
+          </ol>
+        </div>
+      </section>
+    </>
+  );
+}
