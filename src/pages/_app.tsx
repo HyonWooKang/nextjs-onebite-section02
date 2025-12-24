04e09f1d0397dd20481bf447b75dca3062cf564c
HyonWooKang <87benzenes@gmail.com>
2025-12-24 18:04:00 +0900

    화면 이동 코드 작성 by Link, router

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,28 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const router = useRouter();
+  const onClickButton = () => {
+    router.push("/test"); // react와 동일하게 replace(), back() 사용 가능
+  };
+
+  return (
+    <>
+      <header>
+        <Link href={"/"}>index</Link>
+        &nbsp;
+        <Link href={"/search"}>search</Link>
+        &nbsp;
+        <Link href={"/book/1"}>book</Link>
+        <div>
+          <button onClick={onClickButton}>/test 페이지로 이동</button>
+        </div>
+      </header>
+      <Component {...pageProps} />
+    </>
+  );
 }
+// a 태그는 서버사이드 렌더링 -> Link로 클라이언트 사이드 렌더링
