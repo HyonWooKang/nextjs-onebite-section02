708d38da04a81ec6141e42021a1c5fd592b69f84
HyonWooKang <87benzenes@gmail.com>
2025-12-29 02:28:20 +0900

    Global Layout 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,34 +1,11 @@
+import GlobalLayout from "@/components/global-layout";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import Link from "next/link";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  const router = useRouter();
-  const onClickButton = () => {
-    router.push("/test"); // react와 동일하게 replace(), back() 사용 가능
-  };
-
-  useEffect(() => {
-    router.prefetch("/test"); // programmatic routing된 test 페이지의 prefetching 설정
-  }, []);
-
   return (
-    <>
-      <header>
-        <Link href={"/"}>index</Link>
-        &nbsp;
-        <Link href={"/search"} prefetch={false}>
-          search
-        </Link>
-        &nbsp;
-        <Link href={"/book/1"}>book</Link>
-        <div>
-          <button onClick={onClickButton}>/test 페이지로 이동</button>
-        </div>
-      </header>
+    <GlobalLayout>
       <Component {...pageProps} />
-    </>
+    </GlobalLayout>
   );
 }
